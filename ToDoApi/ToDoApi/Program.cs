a6380be740497fe152361fc160483e15747bfc85
CosminManu <cosmin.manu2000@gmail.com>
2024-06-13 13:10:27 +0300

    get rid of comments, deleted weatherforecast default implementation, installed nuget packages EntityFrameworkCore.Design and EntityFrameworkCore.Sqlite

--- ToDoApi/ToDoApi/Program.cs
+++ ToDoApi/ToDoApi/Program.cs
@@ -1,34 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection(); 
+//if you hit 'http' endpoint it automatically sends you to 'https'; good to use http for android endpoints
+//https can be used in dev environment, just a bit extra work to configure
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "ce inseamna asta"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
