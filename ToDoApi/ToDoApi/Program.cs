e5ddef99654ce6e4b6775f600fec96df435085fb
CosminManu <cosmin.manu2000@gmail.com>
2024-06-15 22:39:59 +0300

    added endpoints for get, post; changed launchUrl to api/todos

--- ToDoApi/ToDoApi/Program.cs
+++ ToDoApi/ToDoApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
+using ToDoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,22 @@ var app = builder.Build();
 //if you hit 'http' endpoint it automatically sends you to 'https'; good to use http for android endpoints
 //https can be used in dev environment, just a bit extra work to configure
 
+
+app.MapGet("api/todos", async (AppDbContext ctx) =>
+{
+    var items = await ctx.toDos.ToListAsync();
+
+    return Results.Ok(items);
+});
+
+app.MapPost("api/createToDo", async (AppDbContext ctx, ToDo toDo) =>
+{
+    await ctx.toDos.AddAsync(toDo);
+
+    await ctx.SaveChangesAsync();
+
+    return Results.Created($"api/createToDo/{toDo.Id}", toDo);
+});
 
 
 app.Run();
