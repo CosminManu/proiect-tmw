4ce177e0531c7e8aa90ca9affcc5a041263c10c2
CosminManu <cosmin.manu2000@gmail.com>
2024-06-16 22:32:58 +0300

    initialized maui project

---
+++ ToDoMauiCLient/ToDoMauiCLient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoMauiCLient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ToDoMauiCLient/ToDoMauiCLient/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ToDoMauiCLient
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
